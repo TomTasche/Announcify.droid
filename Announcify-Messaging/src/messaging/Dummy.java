98e03c129fb22bb4323146c2ce74a7a2590ee102
AndyScherzinger <info@andy-scherzinger.de>
2013-02-26 01:22:36 +0100

    not committed

---
+++ Announcify-Messaging/src/messaging/Dummy.java
@@ -0,0 +1,14 @@
+package messaging;
+
+/*
+ * In order to prevent errors after cloning the repository from git
+ */
+public class Dummy {
+
+	private Dummy() {
+		if (com.announcify.plugin.message.sms.activity.SettingsActivity.class != null) {
+		}
+		if (com.announcify.plugin.message.mms.activity.SettingsActivity.class != null) {
+		}
+	}
+}

938b67878208e68e9af40bcb627a9d8f5a7722f3
Thomas Taschauer <tomtasche@gmail.com>
2013-02-27 20:49:33 +0100

    remove gen folders, localize read string for new unread mails

--- K9-Announcify/gen/com/announcify/plugin/mail/k9/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.announcify.plugin.mail.k9;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Mail-Announcify/gen/com/announcify/plugin/mail/google/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.announcify.plugin.mail.google;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
